29eed7268c5e9da1f092590a85430aa2974c5ed3
Rizvan <imeandmyep2@gmail.com>
2023-07-08 14:09:54 +0300

    typed reducer

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,13 +4,39 @@ interface RepositoriesState {
   data: string[];
 }
 
-const reducer = (state: RepositoriesState, action: any) => {
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOS;
+}
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOS_SUCCESS;
+  payload: string[];
+}
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPOS_ERROR;
+  payload: string;
+}
+
+type UnionAction =
+  | SearchRepositoriesErrorAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesAction;
+
+enum ActionType {
+  SEARCH_REPOS = "search_repositories",
+  SEARCH_REPOS_SUCCESS = "search_repositories_success",
+  SEARCH_REPOS_ERROR = "search_repositories_error",
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action: UnionAction,
+): RepositoriesState => {
   switch (action.type) {
-    case "search_repositories":
+    case ActionType.SEARCH_REPOS:
       return { loading: true, error: null, data: [] };
-    case "search_repositories_success":
+    case ActionType.SEARCH_REPOS_SUCCESS:
       return { loading: false, error: null, data: action.payload };
-    case "search_repositories_error":
+    case ActionType.SEARCH_REPOS_ERROR:
       return { loading: false, error: action.payload, data: [] };
     default:
       return state;
